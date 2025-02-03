4d7b8ff06bf6380178a92c24cc18f72bb7a371ef
Deyan Lafchiev <dlafchiev@gmail.com>
2025-02-03 10:59:23 +0200

    Add files via upload

--- API/addNote.php
+++ API/addNote.php
@@ -1,21 +1,16 @@
 <?php
 header('Content-Type: application/json');
-
-// Optional: Set the error reporting level (E_ALL logs all types of errors)
 error_reporting(E_ALL);
 
-// Allow PUT request
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     http_response_code(405);
     echo json_encode(['message' => 'Only POST requests are allowed']);
     exit();
 }
 
-// Read the PUT request body
 $input = json_decode(file_get_contents('php://input'), true);
 
-// Validate input
-if (!isset($input['note_id']) || !isset($input['user_id']) || !isset($input['text']) || !isset($input['dateCreated']) || !isset($input['dateModified'])) {
+if (!isset($input['note_id'], $input['user_id'], $input['text'], $input['dateCreated'], $input['dateModified'])) {
     http_response_code(400);
     echo json_encode(['message' => 'Invalid input']);
     exit();
@@ -26,10 +21,9 @@ $user_id = $input['user_id'];
 $text = $input['text'];
 $dateCreated = $input['dateCreated'];
 $dateModified = $input['dateModified'];
+$folder_id = isset($input['folderId']) && !empty($input['folderId']) ? strtoupper($input['folderId']) : null;
 
-// Connect to PostgreSQL database
 include '../conn_db.php';
-
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
@@ -39,18 +33,29 @@ if (!$conn) {
 }
 
 
-// Assuming $conn is your PostgreSQL connection
-$query = "INSERT INTO data (note_id, user_id, text, date_created, date_modified) VALUES ($1, $2, $3, $4, $5)";
-$result = pg_query_params($conn, $query, [$note_id, $user_id, $text, $dateCreated, $dateModified]);
-
-// Check if the insertion was successful
-if ($result) {
-    echo "Note inserted successfully.";
-} else {
-    echo "Error inserting note: " . pg_last_error($conn);
+if ($folder_id) {
+    $check_folder_query = "SELECT COUNT(*) FROM folders WHERE id = $1";
+    $check_folder_result = pg_query_params($conn, $check_folder_query, [$folder_id]);
+    
+    if ($check_folder_result) {
+        $folder_count = pg_fetch_result($check_folder_result, 0, 0);
+        if ($folder_count == 0) {
+            $folder_id = null; // Folder does not exist, set to NULL
+        }
+    }
 }
 
+$query = "INSERT INTO data (note_id, user_id, text, date_created, date_modified, folder_id) 
+          VALUES ($1, $2, $3, $4, $5, $6)";
+$params = [$note_id, $user_id, $text, $dateCreated, $dateModified, $folder_id];
 
-// Close the database connection
+$result = pg_query_params($conn, $query, $params);
+
+if ($result) {
+    echo json_encode(['message' => 'Note inserted successfully.']);
+} else {
+    echo json_encode(['error' => 'Error inserting note: ' . pg_last_error($conn)]);
+}
+
 pg_close($conn);
 ?>
